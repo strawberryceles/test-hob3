c7bcc953416b82f64a1faa81e85b23baeef2e8e3
strawberryceles <strawberryceles@users.noreply.github.com>
2016-11-20 00:54:53 -0400

    :stars:
    Tercera prueba

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,77 +1,46 @@
 $(document).ready(function(){
-	$('.btn-1').click(function()){
+	$(".play-start").click(function(){
+		$(".stop-click").toggle();
+	})
 
-	});
-
-	$('#submit').click(function()){
-
-	});
-
-	$( "[type='submit']" ).button();
-
-	// $(".overlay").mouseenter(function(){
-	//  $(this).fadeTo("fast",0.5);
-	// });
-	// $(".overlay").mouseleave(function(){
-	//  $(this).fadeTo("slow",0);
-	// });
-
-// var social_networks = [
-//     'wcd_facebook',
-//     'wcd_youtube',
-//     'wcd_twitter',
-//     'wcd_vimeo',
-//     'wcd_soundcloud',
-//     'wcd_instagram'
-// ];
-
+$("#volume").click(function(){
+	$("#vol").toggleClass("active");
 });
 
+$("#repeat,#shuffle").click(function(){
+	$(this).toggleClass("active");
+});
 
------------------------------------
-$(document).ready(function () {
-	$('#boton01').click(function(){
-		if($('#menu').hasClass('open')){
-			$('#menu').removeClass('open');
-			$('#boton01').removeClass('open');
-		}else{
-			$('#menu').addClass('open');
-			$('#boton01').addClass('open');
-			$('#menu2').removeClass('open');
-			$('#boton02').removeClass('open');
-			$('#menu3').removeClass('open');
-			$('#boton03').removeClass('open');
+$(".btn-1").on({
+		click:function(){
+			$("div-1").toggle();
 		}
-	});
-	$('#boton02').click(function(){
-		if($('#menu2').hasClass('open')){
-			$('#menu2').removeClass('open');
-			$('#boton02').removeClass('open');
-		}else{
-			$('#menu2').addClass('open');
-			$('#boton02').addClass('open');
-			$('#menu').removeClass('open');
-			$('#boton01').removeClass('open');
-			$('#menu3').removeClass('open');
-			$('#boton03').removeClass('open');
-			
+		mouseenter:function(){
+			$("p").show("slow");
 		}
-	});
-	$('#boton03').click(function(){
-		if($('#menu3').hasClass('open')){
-			$('#menu3').removeClass('open');
-			$('#boton03').removeClass('open');
-		}else{
-			$('#menu3').addClass('open');
-			$('#boton03').addClass('open');
-			$('#menu').removeClass('open');
-			$('#boton01').removeClass('open');
-			$('#menu2').removeClass('open');
-			$('#boton02').removeClass('open');
-			
+		mouseleave:function(){
+			$("p").hide("slow");
 		}
-	});
-	$('.cerrar').click(function(){
-		$('#menu, #menu2, #menu3').removeClass('open');
-	});
+	}
+
+    $("button").click(function(){
+        $("p").toggle();
+    });
+    
+gfgfg
+    $("p").hide();
+
+    $("button").click(function(){
+        $("p").show();
+    });
+
+    FFd
+
+        $("p").click(function(){
+        $(this).hide("slow");
+    });
+
+
+
+
 });
